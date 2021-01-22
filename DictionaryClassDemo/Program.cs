54e4ad8bd2678d3bd3e042429e7dc4ae02b6245c
Hüseyin Kaya <kayahuseyin15@gmail.com>
2021-01-22 03:55:02 +0300

    Add project files.

---
+++ DictionaryClassDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DictionaryClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, dynamic> users = new MyDictionary<string, dynamic>();
+            users.Add("Ad", "Hüseyin");
+            users.Add("Id", 123);
+
+            foreach (var item in users.Keys)
+            {
+                Console.WriteLine("Key: "+item);
+            }
+            foreach (var item in users.Values)
+            {
+                Console.WriteLine("Value: "+item);
+            }
+
+            users.Dict();
+        }
+    }
+}
